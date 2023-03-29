55b58ae9124d4b8f2b9f72d52ff06cd54f143544
Prabhneet Narula <imailcode@gmail.com>
2023-03-29 18:57:15 +0530

    Learned classes and objects concept

---
+++ classes_and_objects.java
@@ -0,0 +1,18 @@
+class Chicken {
+    public int ingridients(Boolean water, String masala, int chicken, String sabzi) {
+        System.out.println("Do we need water? " + water);
+        System.out.println("We also need " + masala + ", " + chicken + "kg chicken, & " + sabzi + " sabziyaan");
+
+        return 10;
+    }
+}
+
+public class classes_and_objects {
+    public static void main(String[] args) {
+        Chicken butterChicken = new Chicken();
+        butterChicken.ingridients(true, "Mirch, Haldi ,Namak, Chicken Masala", 2, "Pyaaz, Tamatar");
+
+        Chicken kadaiChicken = new Chicken();
+        kadaiChicken.ingridients(true, "Soya Sauce, Mirch, Cream, ", 3, "Shimla Mirch, Tamatar, Pyaar, Kaaju");
+    }
+}
--- print_first_trial.java
+++ print_first_trial.java
@@ -1,5 +1,6 @@
 public class print_first_trial {
     public static void main(String[] args) {
         System.out.println("Hello Everyone!");
+        System.out.println("Hello Animesh");
     }
 }
