7e095dbce2b8d961632c7c4cd65d61e85b3d5fa0
Ashutoshshah707 <129272218+Ashutoshshah707@users.noreply.github.com>
2023-03-31 16:44:01 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,29 @@
+public class Method{
+    public static void main(String[] args) {
+        
+    
+
+    public void ingredients(boolean water,int chicken, String stuff, String sabzi  );
+     System.out.println("do we need water?"+ water);
+     System.out.println("we also need"+stuff+","+ "and vegetables like-"+ sabzi);
+
+
+    
+        
+
+    }    
+}
+    
+     
+
+public class Chicken{
+    public static void main(String[] args) {
+        system.out.printl("Today we are going to make chicken ");
+        chicken butter =new Method();
+        chicken butter.ingredients(true,3,"haldi,mirch,garlic,butter, chicken masal ", "onion tamato "); 
+         
+        chilli chicken= new Method();
+        chilli chicken.ingredients(false,5,"haldi chilli powder, onion ", "onion, ginger, garlic"); 
+
+    }
+}
