f0451bfdfc46263179d7ad3d1aaea788bb5674d0
Prabhneet Narula <imailcode@gmail.com>
2023-04-03 15:36:05 +0530

    value_types done

---
+++ value_and_types.java
@@ -0,0 +1,35 @@
+public class value_and_types { // Boiler Code
+    public static void main(String[] args) {
+        System.out.print("Hello Class 10th\n");
+        System.out.print("Hellooo zunaid \nfrom Mantram Academy\n");
+        System.out.println("Hellooo, Ashutosh");
+        System.out.print("Hello, Bach\tooo");
+
+        System.out.print("\n\n");
+
+        System.out.println("*");
+        System.out.println("* *");
+        System.out
+                .println("* * *");
+        System.out.println("* * * *");
+
+        System.out.println("Ashutosh said, \"Bhyia, Teach me comp\" ");
+
+        int a = 20;
+        int b = 10;
+        System.out.println(2 * (a + b));
+
+        a = 30;
+        b = 20;
+
+        System.out.println(2 * (a + b));
+
+        String name = "Superman";
+        double marks = 79.5;
+        int litre = 5;
+
+        name="Batman";
+        System.out.println(name);
+
+    }
+}
