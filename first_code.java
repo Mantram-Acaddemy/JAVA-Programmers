acd4d76ae3562c79ca844fd68abbe23db5a1a46d
Animesh08Git <gitanimesh438@gmail.com>
2023-04-06 20:54:30 +0530

    done with these codes

---
+++ first_code.java
@@ -0,0 +1,5 @@
+public class first_code {
+    public static void main(String[] args) {
+        System.out.println("first code");
+    }
+}
---
+++ print_first_trial.java
@@ -0,0 +1,5 @@
+public class print_first_trial {
+    public static void main(String[] args) {
+        System.out.println("hello everyone");
+    }
+}
