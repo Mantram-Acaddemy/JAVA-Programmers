d25875106b864028d4e61165bcb5f817b5cc0d80
Animesh08Git <gitanimesh438@gmail.com>
2023-04-09 08:00:53 +0530

    fixed

--- class_work.java
+++ class_work.java
@@ -7,16 +7,19 @@ public class class_work {
         System.out.println("Enter the int =");
 
         int num = test.nextInt();
+        System.out.println("integer="+num);
 
         System.out.println("Enter the double=");
 
         double decimal = test.nextDouble();
+        System.out.println("double="+decimal);
 
         test.nextLine();
 
         System.out.println("Enter the string=");
 
         String word = test.nextLine();
+        System.out.println("string="+word);
 
         test.close();
 
